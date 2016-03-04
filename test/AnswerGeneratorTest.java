4aabf3ecdcb546dd3851778e7c5e8dd88a9b0e55
crystal <793832732@qq.com>
2016-03-04 13:37:21 +0800

    formate should_return_fout_digits_String

--- test/AnswerGeneratorTest.java
+++ test/AnswerGeneratorTest.java
@@ -5,11 +5,13 @@ import static org.hamcrest.Matchers.is;
 
 public class AnswerGeneratorTest {
 
-        @Test
-        public void should_return_fout_digits_String() {
-            AnswerGenerator answerGenerator = new AnswerGenerator();
-            int length = answerGenerator.generateNumber().length();
-            assertThat(length,is(4));
-        }
-    
+    @Test
+    public void should_return_fout_digits_String() {
+        AnswerGenerator answerGenerator = new AnswerGenerator();
+        int length = answerGenerator.generateNumber().length();
+        assertThat(length,is(4));
+    }
+
+
+
 }
