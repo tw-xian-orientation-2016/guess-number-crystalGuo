cda918861f8edf2d4613829be14c62d88d923785
crystal <793832732@qq.com>
2016-03-04 14:03:01 +0800

    modify should_return_fout_digits_String

--- test/AnswerGeneratorTest.java
+++ test/AnswerGeneratorTest.java
@@ -1,17 +1,22 @@
 import org.junit.Test;
 
+import java.util.Random;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class AnswerGeneratorTest {
 
     @Test
     public void should_return_fout_digits_String() {
         AnswerGenerator answerGenerator = new AnswerGenerator();
-        int length = answerGenerator.generateNumber().length();
-        assertThat(length,is(4));
+
+        Random random = mock(Random.class);
+        when(random.nextInt(10)).thenReturn(1, 2, 3, 4);
+        answerGenerator.setRandom(random);
+
+        assertThat(answerGenerator.generateNumber(),is("1234"));
     }
-
-
-
 }
