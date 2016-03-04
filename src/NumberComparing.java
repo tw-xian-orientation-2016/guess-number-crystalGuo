09361e2ac0d54ca8190269188f670f34a8daa553
crystal <793832732@qq.com>
2016-03-04 11:26:19 +0800

    rename couttA and countB

--- src/NumberComparing.java
+++ src/NumberComparing.java
@@ -1,17 +1,17 @@
 public class NumberComparing {
 
     public String compareNumber(String input, String result) {
-        int countA = 0;
-        int countB = 0;
+        int A = 0;
+        int B = 0;
 
         for(int i = 0; i < input.length(); i++) {
             if(input.charAt(i) == result.charAt(i)) {
-                countA ++;
+                A ++;
             } else if(result.contains(String.valueOf(input.charAt(i)))) {
 
             }
         }
 
-        return countA + "A" + countB + "B";
+        return A + "A" + B + "B";
     }
 }
